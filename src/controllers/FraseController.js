b38bdab6478525dd3add618de7649546432f9a3b
Leonardo <leonardo.fc2@outlook.com>
2019-08-11 03:26:59 -0300

    feature: return random frase

--- src/controllers/FraseController.js
+++ src/controllers/FraseController.js
@@ -1,19 +1,23 @@
 const Frase = require('../models/Frase') 
 
-
 module.exports={
     index(require,response){
-        console.log('entrou no index')
-
-        return response.json(require.body)
+        Frase.countDocuments().exec(async function (err, count){
+            const random = await  Math.floor(Math.random() * count)
+            await Frase.findOne().skip(random).exec(
+                function (err, result) {
+                    return response.json({FraseAleatoria:result})
+                })
+        })
     },
     async store(require,response){
-        await console.log('entrou no store')
-        console.log(require.body)
-        awa
-        return response.json(require.body)
-
+        const {content, author} = require.body
+        const frase = await Frase.create({
+            content,
+            author
+        })
+        await console.log(frase)
+        return response.json({ok:`frase salva com sucesso com o id:${frase._id}`})
     }
-    
 }
 
